143f99f46fb41e29e2d573c5588cef7ef8033d14
erthalion <9erthalion6@gmail.com>
2013-12-11 21:12:07 +0700

    Introduced generic class usage

--- src/Models.hs
+++ src/Models.hs
@@ -1,28 +1,21 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Models where
 
 import qualified Data.ByteString.Char8 as BS
 import Data.Aeson ((.:), FromJSON(..), Value(..))
 import Control.Applicative ((<$>), (<*>))
+import GHC.Generics (Generic)
 
 data CommitInfo = CommitInfo {
     message :: BS.ByteString
-    } deriving (Show)
+    } deriving (Show, Generic)
 
 data Commit = Commit {
     sha :: BS.ByteString,
     url :: BS.ByteString,
     commit :: CommitInfo
-    } deriving (Show)
+    } deriving (Show, Generic)
 
-instance FromJSON Commit where
-    parseJSON (Object v) =
-        Commit         <$>
-        (v .: "sha")   <*>
-        (v .: "url")   <*>
-        (v .: "commit")
-
-instance FromJSON CommitInfo where
-    parseJSON (Object v) =
-        CommitInfo       <$>
-        (v .: "message")
+instance FromJSON Commit
+instance FromJSON CommitInfo
